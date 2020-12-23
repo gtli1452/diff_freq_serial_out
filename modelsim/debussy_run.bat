a82f463fd1d2453094f7f7f3eb824d1747bc4e1f
Tim.Li <silentbrain1452@gmail.com>
2020-12-23 13:59:38 +0800

    Add UART function

--- modelsim/debussy_run.bat
+++ modelsim/debussy_run.bat
@@ -9,6 +9,9 @@ debussy -2001 ./../tb/diff_freq_serial_out_tb.v ^
               ./../rtl/diff_freq_serial_out.v   ^
               ./../rtl/serial_out.v             ^
               ./../rtl/mod_m_counter.v          ^
+              ./../rtl/uart.v                   ^
+              ./../rtl/uart_tx.v                ^
+              ./../rtl/uart_rx.v                ^
               -ssf diff_freq_serial_out.fsdb    ^
               -sswr waveform.rc
 
