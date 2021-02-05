f3f4e53efd05d56899c8956be13d486ae147d711
Tim.Li <silentbrain1452@gmail.com>
2021-02-05 17:31:45 +0800

    Modify 16  output

--- tb/diff_freq_serial_out_test.v
+++ tb/diff_freq_serial_out_test.v
@@ -7,14 +7,26 @@ Release     : 12/16/2020 v1.0
 */
 
 module diff_freq_serial_out_test (
-  input  clk,           // PIN_P9
-  input  rst_n,         // PIN_G13
-  output o_serial_out0, // PIN_P4
-  output o_serial_out1, // PIN_R4
-  output o_serial_out2, // PIN_R6
-  output o_serial_out3, // PIN_T7
-  output o_bit_tick,    // PIN_R10
-  output o_done_tick,   // PIN_T8
+  input  clk,            // PIN_P9
+  input  rst_n,          // PIN_G13
+  output o_serial_out0,  // PIN_B6
+  output o_serial_out1,  // PIN_B7
+  output o_serial_out2,  // PIN_B8
+  output o_serial_out3,  // PIN_B10
+  output o_serial_out4,  // PIN_C10
+  output o_serial_out5,  // PIN_A12
+  output o_serial_out6,  // PIN_A13
+  output o_serial_out7,  // PIN_A14
+  output o_serial_out8,  // PIN_B15
+  output o_serial_out9,  // PIN_C15
+  output o_serial_out10, // PIN_D14
+  output o_serial_out11, // PIN_D13
+  output o_serial_out12, // PIN_E15
+  output o_serial_out13, // PIN_G16
+  output o_serial_out14, // PIN_H16
+  output o_serial_out15, // PIN_J16
+  output o_bit_tick,     // PIN_R10
+  output o_done_tick,    // PIN_T8
   // UART
   input  i_rx,          // PIN_K12
   output o_tx,          // PIN_M12
@@ -39,10 +51,22 @@ wire [7:0]  rx_received_data;
 wire        o_rx_done_tick, o_tx_done_tick;
 wire [15:0] o_serial_out;
 
-assign o_serial_out0 = o_serial_out[0];
-assign o_serial_out1 = o_serial_out[1];
-assign o_serial_out2 = o_serial_out[2];
-assign o_serial_out3 = o_serial_out[3];
+assign o_serial_out0  = o_serial_out[0];
+assign o_serial_out1  = o_serial_out[1];
+assign o_serial_out2  = o_serial_out[2];
+assign o_serial_out3  = o_serial_out[3];
+assign o_serial_out4  = o_serial_out[4];
+assign o_serial_out5  = o_serial_out[5];
+assign o_serial_out6  = o_serial_out[6];
+assign o_serial_out7  = o_serial_out[7];
+assign o_serial_out8  = o_serial_out[8];
+assign o_serial_out9  = o_serial_out[9];
+assign o_serial_out10 = o_serial_out[10];
+assign o_serial_out11 = o_serial_out[11];
+assign o_serial_out12 = o_serial_out[12];
+assign o_serial_out13 = o_serial_out[13];
+assign o_serial_out14 = o_serial_out[14];
+assign o_serial_out15 = o_serial_out[15];
 
 // Data register
 always @(posedge clk) begin
